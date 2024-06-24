04de42fc9bb675b93f012a309118eb2504b2924d
pezzedelaa <129279646+pezzedelaa@users.noreply.github.com>
2024-06-24 11:20:39 -0300

    Add files via upload

---
+++ xuxu-bolado-1906-main/xuxu bolado/scripit.js
@@ -0,0 +1,13 @@
+const caixaPrincipal=Documento.querySelector('.caixa-principal');
+const caixaPerguntas=Documento.querySelector('.caixa-perguntas');
+const caixaAlternativas=Documento.querySelector('.caixa-alternativas');
+const caixaResultado=Documento.querySelector('.caixa-resultado');
+
+
+const perguntas=[
+    enunciado "pergunta 1"
+]
+
+
+
+
