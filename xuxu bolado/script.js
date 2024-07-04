7e13de5d01855c67dd85c4bc0ae5513b6fe5768c
para de deixar a aba aberta retardado <129279646+pezzedelaa@users.noreply.github.com>
2024-07-04 08:40:45 -0300

    Add files via upload

---
+++ xuxu bolado/script.js
@@ -0,0 +1,59 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "vocẽ ja jogou alguma vez ?",
+        alternativas: [
+            "sim!",
+            "não!",
+        ]
+    },
+    {
+        enunciado: "Já perdeu muito dinheiro com o tigrito ?",
+        alternativas: [
+            "sim.",
+            "não.",
+        ]
+    },
+    {
+        enunciado: "?",
+        alternativas: [
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "Criar uma imagem utilizando um gerador de imagem de IA."
+        ]
+    },
+   
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
+
+
